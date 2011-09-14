d37d6373315275b62612abc15a6b2db74a74305d
sunookitsune@kitsunet.org <sunookitsune@kitsunet.org@26520ff5-492d-4116-9945-e049a8b45a10>
2011-09-14 14:23:45 +0000

    Fixed a few bugs and split out a Configuration file.

---
+++ Outlook2iCal/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Outlook2iCal
+{
+    class Configuration
+    {
+        public static readonly string FtpUrl = "ftp://SERVER/PATH/outlook.ics";
+        public static readonly string FtpUser = "USERNAME";
+        public static readonly string FtpPass = "PASSWORD";
+
+        public static readonly string MailDomain = "@example.com";
+
+        public static readonly bool IncludeClass = false;
+    }
+}
